348b5ec211665921a9148cd911122c4479602357
danish <danishabbas2004@gmail.com>
2025-08-09 11:01:35 +0500

    Adding the tracker schema

--- src/models/DietModel.ts
+++ src/models/DietModel.ts
@@ -1,43 +1,41 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IDiet extends Document {
-    foodName: string;
-    userId: mongoose.Types.ObjectId;
-    meal: "breakfast" | "lunch" | "dinner" | "snack";
-    calories: number;
-    carbs: number;
-    protein: number;
-    fat: number;
-    status: "taken" | "next" | "overdue" | "skipped";
-    goalId?: mongoose.Types.ObjectId;
-    createdAt: Date;
-    updatedAt: Date;
+  foodName: string;
+  userId: mongoose.Types.ObjectId;
+  meal: "breakfast" | "lunch" | "dinner" | "snack";
+  calories: number;
+  carbs: number;
+  protein: number;
+  fat: number;
+  mealWeight: number;
+  mealDateAndTime: Date;
+  goalId?: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const DietSchema = new mongoose.Schema(
-    {
-        foodName: { type: String, required: true },
-        userId: { type: mongoose.Types.ObjectId, required: true, ref: 'User' },
-        meal: {
-            type: String,
-            enum: ["breakfast", "lunch", "dinner", "snack"],
-            required: true,
-        },
-        calories: { type: Number, required: true },
-        carbs: { type: Number, required: true },
-        protein: { type: Number, required: true },
-        fat: { type: Number, required: true },
-        status: {
-            type: String,
-            enum: ["taken", "next", "overdue", "skipped"],
-            default: "next",
-        },
-        goalId: { type: mongoose.Types.ObjectId, ref: 'Goal' },
+  {
+    foodName: { type: String, required: true },
+    userId: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+    meal: {
+      type: String,
+      enum: ["breakfast", "lunch", "dinner", "snack"],
+      required: true,
     },
-    {
-        timestamps: true, // ⏱ Automatically adds `createdAt` and `updatedAt`
-    }
+    calories: { type: Number, required: true },
+    carbs: { type: Number, required: true },
+    protein: { type: Number, required: true },
+    fat: { type: Number, required: true },
+    mealDateAndTime: { type: Date, required: true },
+    mealWeight: { typ: Number },
+    goalId: { type: mongoose.Types.ObjectId, ref: "Goal" },
+  },
+  {
+    timestamps: true, // ⏱ Automatically adds `createdAt` and `updatedAt`
+  }
 );
 DietSchema.index({ userId: 1, meal: 1, foodName: 1 }, { unique: true });
 
-export const Diet = mongoose.model<IDiet>('Diet', DietSchema);
+export const Diet = mongoose.model<IDiet>("Diet", DietSchema);
